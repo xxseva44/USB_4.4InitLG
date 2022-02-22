561795e9de7e7eaf7ba9ac68a33360aaa5c89a12
xxseva44 <dev.xxseva44@gmail.com>
2022-02-21 19:06:11 -0500

    Initial set up

--- customize.sh
+++ customize.sh
@@ -11,8 +11,8 @@
 # Uncomment and change 'MINAPI' and 'MAXAPI' to the minimum and maximum android version for your mod
 # Uncomment DYNLIB if you want libs installed to vendor for oreo+ and system for anything older
 # Uncomment DEBUG if you want full debug logs (saved to /sdcard)
-#MINAPI=21
-#MAXAPI=25
+MINAPI=28
+MAXAPI=31
 #DYNLIB=true
 #DEBUG=true
 
@@ -23,17 +23,11 @@
 # List all directories you want to directly replace in the system
 # Check the documentations for more info why you would need this
 
-# Construct your list in the following format
-# This is an example
-REPLACE_EXAMPLE="
-/system/app/Youtube
-/system/priv-app/SystemUI
-/system/priv-app/Settings
-/system/framework
-"
-
 # Construct your own list here
 REPLACE="
+/vendor/etc/init/hw/init.qcom.usb.rc
+/vendor/etc/init/hw/init.qcom.rc
+/vendor/etc/init/hw/init.msm.usb.configfs.rc
 "
 
 ##########################################################################################
